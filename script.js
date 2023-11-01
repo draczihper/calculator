5461c6570505a0d8b6f5ed88b174df532ea6eb99
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-11-01 23:41:04 +0300

    Add operations functions

---
+++ script.js
@@ -0,0 +1,16 @@
+// OPERATIONS
+const add = function (a, b) {
+    return a + b;
+}
+
+const subtract = function (a, b) {
+    return a + b;
+}
+
+const multiply = function (a, b) {
+    return a * b;
+}
+
+const divide = function (a, b) {
+    return a / b;
+}
