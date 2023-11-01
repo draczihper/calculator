82098ff686de9e2f3a6c4d92d0fb883c1fcce495
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-11-02 00:28:18 +0300

    Add number variables and operate function

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
     return a / b;
 }
+
+let num1;
+let operator;
+let num2;
+
+const operate = function (operator, num1, num2) {
+    switch (operator) {
+        case operator == '+':
+            add(num1, num2);
+            break;
+        case operator == '-':
+            subtract(num1, num2);
+            break;
+        case operator == '*':
+            multiply(num1, num2);
+            break;
+        case operator == '/':
+            divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
