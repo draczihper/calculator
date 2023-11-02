2c513f3cfe6864583a55e8766b6b22cb0f7b1efc
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-11-03 01:26:32 +0300

    Add keyboard input functionality

--- script.js
+++ script.js
@@ -43,45 +43,65 @@ const toDisplay = function () {
     let answerDisplay = document.querySelector('#answer');
     const digitButtons = document.querySelectorAll('.digits');
     const dotButton = document.querySelector('#dot');
+    const operatorButtons = document.querySelectorAll('.operators');
     let hasDot = false;
+
+    function handleDigitInput(digitToDisplay) {
+        if (digitToDisplay === '.' && hasDot) {
+            return;
+        }
+        if (digitToDisplay === '.') {
+            hasDot = true;
+            dotButton.disabled = true;
+        }
+        operationDisplay.textContent += digitToDisplay;
+        currentNum = operationDisplay.textContent;
+    }
     digitButtons.forEach((digitButton) => {
         digitButton.addEventListener('click', () => {
-            const digitToDisplay = digitButton.textContent; 
-            if (digitToDisplay === '.' && hasDot) {
-                return;
-            }
-            if(digitToDisplay === '.') {
-                hasDot = true;
-                dotButton.disabled = true;
-            }
-            operationDisplay.textContent += digitToDisplay;
-            currentNum = operationDisplay.textContent;
+            const digitToDisplay = digitButton.textContent;
+            handleDigitInput(digitToDisplay);
         });
     });
-    const operatorButtons = document.querySelectorAll('.operators');
+
+    function handleOperator (operatorToDisplay) {
+        if (lastNum === '') {
+            lastNum = currentNum;
+            if (operatorToDisplay === '*'){
+                operator = 'x';
+            } else if (operatorToDisplay === '/') {
+                operator = '÷';
+            } else {
+                operator = operatorToDisplay;
+            }
+            answerDisplay.textContent = lastNum + operator;
+            operationDisplay.textContent = ''
+        } else {
+            if (operator !== '') {
+                intermediateResult = operate(operator, lastNum, currentNum);
+                if (operator === '÷' && currentNum == 0) {
+                    operationDisplay.textContent = 'You can not divide by 0!';
+                } else {
+                    const roundedIntermediateResult = Math.round(intermediateResult * 1000) / 1000;
+                    lastNum = roundedIntermediateResult;
+                    if (operatorToDisplay === '*'){
+                        operator = 'x';
+                    } else if (operatorToDisplay === '/') {
+                        operator = '÷';
+                    } else {
+                        operator = operatorToDisplay;
+                    }
+                    currentNum = '';
+                    answerDisplay.textContent = intermediateResult + operatorToDisplay;
+                    operationDisplay.textContent = '';
+                }
+            }
+        }
+    }
     operatorButtons.forEach((operatorButton) => {
         operatorButton.addEventListener('click', () => {
             const operatorToDisplay = operatorButton.textContent;
-            if (lastNum === '') {
-                lastNum = currentNum;
-                operator = operatorToDisplay;
-                answerDisplay.textContent = lastNum + operator;
-                operationDisplay.textContent = ''
-            } else {
-                if (operator !== '') {
-                    intermediateResult = operate(operator, lastNum, currentNum);
-                    if (operator === '÷' && currentNum == 0) {
-                        operationDisplay.textContent = 'You can not divide by 0!';
-                    } else {
-                        const roundedIntermediateResult = Math.round(intermediateResult * 1000) / 1000;
-                        lastNum = roundedIntermediateResult;
-                        operator = operatorToDisplay;
-                        currentNum = '';
-                        answerDisplay.textContent = intermediateResult + operatorToDisplay;
-                        operationDisplay.textContent = '';
-                    }
-                }
-            }
+            handleOperator(operatorToDisplay);
         });
     });
     const equalButton = document.querySelector('#equal');
@@ -100,17 +120,39 @@ const toDisplay = function () {
             }
         }
     });
-    const deleteButton = document.querySelector('#delete');
-    deleteButton.addEventListener('click', () => {
+    document.addEventListener('keydown', (event) => {
+        const key = event.key;
+
+        if (/^[0-9.]$/.test(key)) {
+            handleDigitInput(key);
+        } else if (key === '+' || key === '-') {
+            handleOperator(key);
+        } else if (key === '*' || key === '/') {
+            if (key === '/') {
+                event.preventDefault();
+            }
+            handleOperator(key);
+        } else if (key === 'Backspace') {
+            handleDelete();
+        } else if (key === 'Escape') {
+            clearButton.click();
+        } else if (key === 'Enter' || key === '=') {
+            equalButton.click();
+        }
+    });
+    function handleDelete() {
         if (currentNum.length > 0) {
             currentNum = currentNum.slice(0, -1);
-        // } else if (operator > 0){
-        //     operator = '';
+            // } else if (operator){
+            //     operator = '';
         } else if (lastNum > 0) {
             lastNum = lastNum.slice(0, -1);
         }
         operationDisplay.textContent = operationDisplay.textContent.slice(0, -1);
-    });
+    }
+    const deleteButton = document.querySelector('#delete');
+    deleteButton.addEventListener('click', handleDelete);
+
     const clearButton = document.querySelector('#clear');
     clearButton.addEventListener('click', () => {
         lastNum = '';
