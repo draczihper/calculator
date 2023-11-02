60aa680980bce04067c609f04b5a2575be755c57
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-11-02 21:59:08 +0300

    Edit the operate function to operate just one single pair and fix the subtract function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const add = function (a, b) {
 }
 
 const subtract = function (a, b) {
-    return a + b;
+    return a - b;
 }
 
 const multiply = function (a, b) {
@@ -15,36 +15,27 @@ const divide = function (a, b) {
     return a / b;
 }
 
-let num1;
-let operator;
-let num2;
+let lastNum = '';
+let operator = '';
+let currentNum = '';
+let intermediateResult = null;
 
-const operate = function (operator, num1, num2) {
-    num1 = Number(num1);
-    num2 = Number(num2);
+const operate = function (operator, lastNum, currentNum) {
+    lastNum = Number(lastNum);
+    currentNum = Number(currentNum);
     switch (operator) {
         case '+':
-            return add(num1, num2);
+            return add(lastNum, currentNum);
         case '-':
-            return subtract(num1, num2);
+            return subtract(lastNum, currentNum);
         case 'x':
-            return multiply(num1, num2);
+            return multiply(lastNum, currentNum);
         case '÷':
-            return divide(num1, num2);
+            return divide(lastNum, currentNum);
         default:
             return null;
     }
 }
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const operationDisplay = document.querySelector('#operation');
-//     const digitsEl = document.querySelectorAll('.digits');
-//     digitsEl.forEach((digit) => {
-//         digit.addEventListener('click', () => {
-//             console.log(digit.textContent);
-//         });
-//     });
-// });
 
 const toDisplay = function () {
     let operationDisplay = document.querySelector('#operation');
@@ -54,25 +45,41 @@ const toDisplay = function () {
         digitButton.addEventListener('click', () => {
             const digitToDisplay = digitButton.textContent;
             operationDisplay.textContent += digitToDisplay;
-            num2 = operationDisplay.textContent;
+            currentNum = operationDisplay.textContent;
         });
     });
     const operatorButtons = document.querySelectorAll('.operators');
     operatorButtons.forEach((operatorButton) => {
         operatorButton.addEventListener('click', () => {
             const operatorToDisplay = operatorButton.textContent;
-            answerDisplay.textContent = operationDisplay.textContent + operatorToDisplay;
-            operator = operatorToDisplay;
-            num1 = operationDisplay.textContent;
-            operationDisplay.textContent = '';
+            if (lastNum === ''){
+                lastNum = currentNum;
+                operator = operatorToDisplay;
+                answerDisplay.textContent = lastNum + operator;
+                operationDisplay.textContent = ''
+            } else {
+                if (operator !== ''){
+                    intermediateResult = operate(operator, lastNum, currentNum);
+                    lastNum = intermediateResult;
+                    operator = operatorToDisplay;
+                    currentNum = '';
+                    answerDisplay.textContent = intermediateResult + operatorToDisplay;
+                    operationDisplay.textContent = ''
+                }
+            }
         });
     });
     const equalButton = document.querySelector('#equal');
     equalButton.addEventListener('click', () => {
-        answerDisplay.textContent += num2 + equalButton.textContent;
-        operationDisplay.textContent = '';
-        operationDisplay.textContent = operate(operator, num1, num2);
-    })
+        if (lastNum !== '' && currentNum !== '' && operator !== '') {
+            intermediateResult = operate(operator, lastNum, currentNum);
+            operationDisplay.textContent = intermediateResult;
+            answerDisplay.textContent = '';
+            lastNum = intermediateResult;
+            currentNum = '';
+            operator = '';
+        }
+    });
 }
 toDisplay();
 
